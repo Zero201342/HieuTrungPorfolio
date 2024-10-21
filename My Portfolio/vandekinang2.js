dcc5d58cde933fd68d33084043ae53367094b0c4
Zero201342 <tulenhtaungam2025@gmail.com>
2024-10-21 11:41:52 +0700

    Update vandekinang2.js
    chặn ăn cắp nguồn trang

--- My Portfolio/vandekinang2.js
+++ My Portfolio/vandekinang2.js
@@ -57,3 +57,25 @@ document.querySelector('.skillInfoContainer').addEventListener('mouseover', () =
 document.querySelector('.skillInfoContainer').addEventListener('mouseout', () => {
     document.querySelector('.skillInfoContainer').style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.5)';
 });
+
+
+// Chặn F12
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+});
+
+// Chặn Ctrl+U
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'u') {
+        event.preventDefault();
+    }
+});
+
+// Chặn Ctrl+Shift+I (mở DevTools)
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.shiftKey && event.key === 'I') {
+        event.preventDefault();
+    }
+});
